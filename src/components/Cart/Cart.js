09fc328a4cab13c68db8a5b1c898145a44d8e7c1
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-07 23:48:17 +0600

    aa

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart, handleRemoveItem }) => {
+  let message;
+  if (cart.length === 0) {
+    message = <p>Please buy at least one item!!!</p>;
+  }
   return (
     <div>
       <h1>order Summary</h1>
@@ -12,6 +16,7 @@ const Cart = ({ cart, handleRemoveItem }) => {
           <button onClick={() => handleRemoveItem(tshirt)}>X</button>
         </p>
       ))}
+      {message}
     </div>
   );
 };
